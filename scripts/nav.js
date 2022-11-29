4624231950f10f6ab540ddbe1a9cabb2bdde8e4d
Ming Yi <myi@student.ethz.ch>
2022-11-29 17:53:11 +0800

    full page sidebar

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,19 +1,15 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidebar").style.width = "20%";
-    document.getElementById("main").style.marginLeft = "20%";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function changeNav() {
-    const siderbarWidth = document.getElementById("mySidebar").style.width;
-    if (siderbarWidth == "0%") {
+    const display = document.getElementById("mySidebar").style.display;
+    if (!display) {
+      document.getElementById("mySidebar").style.display = "block"
       document.getElementById("navBtn").innerHTML = "&times;";
-      document.getElementById("mySidebar").style.width = "20%";
-      document.getElementById("main").style.marginLeft = "20%";
+      // document.getElementById("mySidebar").style.width = "20%";
+      // document.getElementById("main").style.marginLeft = "20%";
     }else{
       document.getElementById("navBtn").innerHTML = "&#9776;";
-      document.getElementById("mySidebar").style.width = "0%";
-      document.getElementById("main").style.marginLeft = "0%";
+      document.getElementById("mySidebar").style.display = ""
+      // document.getElementById("mySidebar").style.width = "0%";
+      // document.getElementById("main").style.marginLeft = "0%";
     }
   }
