c6b0be5b9db6725c2384d7b55c4d0ba229f40675
Ming Yi <myi@student.ethz.ch>
2022-11-29 17:20:16 +0800

    put open and close together

--- scripts/nav.js
+++ scripts/nav.js
@@ -5,7 +5,15 @@ function openNav() {
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  function changeNav() {
+    const siderbarWidth = document.getElementById("mySidebar").style.width;
+    if (siderbarWidth == "0%") {
+      document.getElementById("navBtn").innerHTML = "&times;";
+      document.getElementById("mySidebar").style.width = "20%";
+      document.getElementById("main").style.marginLeft = "20%";
+    }else{
+      document.getElementById("navBtn").innerHTML = "&#9776;";
+      document.getElementById("mySidebar").style.width = "0%";
+      document.getElementById("main").style.marginLeft = "0%";
+    }
   }
